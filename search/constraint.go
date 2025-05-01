10512c9ad5e4f1b2bc6c1363351fc3b06d34f150
alanprot <alanprot@gmail.com>
2025-04-30 17:15:01 -0700

    refactor on how to read the values + caching values when coming from dictionary
    Signed-off-by: alanprot <alanprot@gmail.com>

--- search/constraint.go
+++ search/constraint.go
@@ -79,6 +79,10 @@ func (s *symbolTable) Get(i int) parquet.Value {
 	}
 }
 
+func (s *symbolTable) GetIndex(i int) int32 {
+	return s.syms[i]
+}
+
 func (s *symbolTable) Reset(pg parquet.Page) {
 	dict := pg.Dictionary()
 	data := pg.Data()
