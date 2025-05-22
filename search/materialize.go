065d258055ddfdea733b811b67d586adaf0f201e
Michael Hoffmann <mhoffmann@cloudflare.com>
2025-05-22 17:08:12 +0000

    materialize: make partitioner work with multiple partitons
    Signed-off-by: Michael Hoffmann <mhoffmann@cloudflare.com>

--- search/materialize.go
+++ search/materialize.go
@@ -17,6 +17,7 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"maps"
 	"slices"
 	"sort"
 	"sync"
@@ -264,15 +265,19 @@ func (m *Materializer) materializeAllLabels(ctx context.Context, rgi int, rr []R
 	return results, nil
 }
 
+func totalRows(rr []RowRange) int64 {
+	res := int64(0)
+	for _, r := range rr {
+		res += r.count
+	}
+	return res
+}
+
 func (m *Materializer) materializeChunks(ctx context.Context, rgi int, mint, maxt int64, rr []RowRange) ([][]chunks.Meta, error) {
 	minDataCol := m.s.DataColumIdx(mint)
 	maxDataCol := m.s.DataColumIdx(maxt)
 	rg := m.b.ChunksFile().RowGroups()[rgi]
-	totalRows := int64(0)
-	for _, r := range rr {
-		totalRows += r.count
-	}
-	r := make([][]chunks.Meta, totalRows)
+	r := make([][]chunks.Meta, totalRows(rr))
 
 	for i := minDataCol; i <= min(maxDataCol, len(m.dataColToIndex)-1); i++ {
 		values, err := m.materializeColumn(ctx, m.b.ChunksFile(), rg, rg.ColumnChunks()[m.dataColToIndex[i]], rr)
@@ -321,21 +326,25 @@ func (m *Materializer) materializeColumn(ctx context.Context, file *storage.Parq
 
 		for _, r := range rr {
 			if pageRowRange.Overlaps(r) {
-				pagesToRowsMap[i] = append(pagesToRowsMap[i], r)
+				pagesToRowsMap[i] = append(pagesToRowsMap[i], pageRowRange.Intersection(r))
 			}
 		}
 	}
 
-	r := make(map[RowRange][]parquet.Value, len(rr))
+	pageRanges := m.coalescePageRanges(pagesToRowsMap, oidx)
+
+	r := make(map[RowRange][]parquet.Value, len(pageRanges))
 	rMutex := &sync.Mutex{}
-	for _, v := range rr {
-		r[v] = []parquet.Value{}
+	for _, v := range pageRanges {
+		for _, rs := range v.rows {
+			r[rs] = make([]parquet.Value, 0, rs.count)
+		}
 	}
 
 	errGroup := &errgroup.Group{}
 	errGroup.SetLimit(m.concurrency)
 
-	for _, p := range m.coalescePageRanges(pagesToRowsMap, oidx) {
+	for _, p := range pageRanges {
 		errGroup.Go(func() error {
 			pgs := file.GetPages(ctx, cc)
 			defer func() { _ = pgs.Close() }()
@@ -389,11 +398,16 @@ func (m *Materializer) materializeColumn(ctx context.Context, file *storage.Parq
 		return nil, errors.Wrap(err, "failed to materialize columns")
 	}
 
-	values := make([]parquet.Value, 0, len(rr))
-	for _, v := range rr {
-		values = append(values, r[v]...)
+	ranges := slices.Collect(maps.Keys(r))
+	slices.SortFunc(ranges, func(a, b RowRange) int {
+		return int(a.from - b.from)
+	})
+
+	res := make([]parquet.Value, 0, totalRows(rr))
+	for _, v := range ranges {
+		res = append(res, r[v]...)
 	}
-	return values, err
+	return res, nil
 }
 
 type pageEntryRead struct {
